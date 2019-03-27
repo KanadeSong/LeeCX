b9d5de0630fc5e4f49c9480595a68dde79bdda7c
kanateSong <545430154@qq.com>
2019-03-27 16:59:08 +0800

    主要代码

--- leecx-mng/leecx-mng-web/src/main/java/com/ljj/web/test/Test.java
+++ leecx-mng/leecx-mng-web/src/main/java/com/ljj/web/test/Test.java
@@ -10,9 +10,5 @@ package com.ljj.web.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
     }
 }

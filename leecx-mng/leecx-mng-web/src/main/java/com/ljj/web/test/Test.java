44606eeb28379d0171ea2cd403d89f59d0b5a31f
kanateSong <545430154@qq.com>
2019-03-27 16:45:33 +0800

    test

---
+++ leecx-mng/leecx-mng-web/src/main/java/com/ljj/web/test/Test.java
@@ -0,0 +1,15 @@
+package com.ljj.web.test;
+
+/**
+ * @CLassName Test
+ * @Description hello world
+ * @Author LeeJack
+ * @Date 2019/3/27/027 16:31
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
